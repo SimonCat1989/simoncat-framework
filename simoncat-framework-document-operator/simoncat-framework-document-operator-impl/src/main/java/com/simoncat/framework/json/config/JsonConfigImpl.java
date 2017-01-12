31f5f00a76d48a7c2de930f779655b6aef222459
LUO Yihao <luo_y@worksap.co.jp>
2017-01-12 18:13:09 +0800

    [Simon LUO] Update the configuration of JsonConfig

--- simoncat-framework-document-operator/simoncat-framework-document-operator-impl/src/main/java/com/simoncat/framework/json/config/JsonConfigImpl.java
+++ simoncat-framework-document-operator/simoncat-framework-document-operator-impl/src/main/java/com/simoncat/framework/json/config/JsonConfigImpl.java
@@ -1,10 +1,15 @@
 package com.simoncat.framework.json.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.simoncat.framework.json.api.JsonOperator;
 import com.simoncat.framework.json.core.JacksonJsonOperator;
 
+@Configuration
 public class JsonConfigImpl implements JsonConfig {
 
+	@Bean
 	@Override
 	public JsonOperator jsonOperator() {
 		return new JacksonJsonOperator();
