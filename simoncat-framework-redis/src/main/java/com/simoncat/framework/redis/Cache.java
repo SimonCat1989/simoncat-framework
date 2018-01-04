630ceb238e9732a273d3c3b7d8c688eaa8d48f8b
LUO Yihao <luo_y@worksap.co.jp>
2018-01-04 17:17:26 +0800

    [Simon LUO] Remove unneeded projects

--- simoncat-framework-redis/src/main/java/com/simoncat/framework/redis/Cache.java
+++
@@ -1,8 +0,0 @@
-package com.simoncat.framework.redis;
-
-public interface Cache<T> {
-
-   T get(String key); 
-   
-   void put(String key, T value);
-}
--- simoncat-framework-redis/src/main/java/com/simoncat/framework/redis/RedisCacheConfig.java
+++
@@ -1,6 +0,0 @@
-package com.simoncat.framework.redis;
-
-//@Configure
-public class RedisCacheConfig {
-
-}
